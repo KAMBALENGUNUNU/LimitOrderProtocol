9441fbe015152fa82d7e8f0ab0f383b15191a50c
daniel <danielngun04@gmail.com>
2025-07-28 16:03:05 +0200

    adding some comments

--- contracts/EnhancedTWAPLimitOrderV2.sol
+++ contracts/EnhancedTWAPLimitOrderV2.sol
@@ -8,3 +8,9 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+/**
+ * @title Enhanced TWAP Limit Order Protocol with 1inch LOP Extensions
+ * @dev Advanced hackathon submission implementing custom extensions for 1inch Limit Order Protocol
+ * @author Daniel KAMBALE - Hackathon Submission
+ */
+ 
