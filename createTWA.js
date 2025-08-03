251112d6a32eb718cc36572b91ead45d49fce6d4
daniel <danielngun04@gmail.com>
2025-08-03 16:29:48 +0200

    onchain test for the create order

--- createTWA.js
+++ createTWA.js
@@ -21,3 +21,20 @@ async function main() {
       contractArtifact.abi,
       user
     );
+
+        // 5. Token addresses
+    const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    
+    // 6. Get complete WETH ABI including all needed functions
+    const wethAbi = [
+      // Standard ERC-20 functions
+      "function balanceOf(address) view returns (uint256)",
+      "function transfer(address to, uint256 amount) external returns (bool)",
+      "function approve(address spender, uint256 amount) external returns (bool)",
+      "function allowance(address owner, address spender) external view returns (uint256)",
+      
+      // WETH-specific functions
+      "function deposit() payable",
+      "function withdraw(uint256 wad)"
+    ];
